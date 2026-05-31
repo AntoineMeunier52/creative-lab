0a8ad7d1e3b50a6883980c12798682070708104f
AntoineMeunier52 <meunier.antoine52@gmail.com>
2026-05-31 15:24:28 +0200

    add import

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,13 @@
 import EventEmitter from "./eventEmitter";
+import globalTicker from "./ticker";
+import { map, lerp, getMousePos, calcWinSize, distance } from "./utils";
 
 export default {
   EventEmitter,
+  globalTicker,
+  map,
+  lerp,
+  getMousePos,
+  calcWinSize,
+  distance,
 };
