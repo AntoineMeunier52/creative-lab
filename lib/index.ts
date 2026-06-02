c3d119447902c1ee8df3ae5c17f247cd81942432
AntoineMeunier52 <meunier.antoine52@gmail.com>
2026-06-03 00:09:21 +0200

    feat: add simplex-noise dependency and update exports in index

--- lib/index.ts
+++ lib/index.ts
@@ -3,4 +3,12 @@ export { default as EventEmitter } from "./eventEmitter";
 export { default as globalTicker } from "./ticker";
 export { default as Tween } from "./tween";
 export type { EasingFunction } from "./type";
-export { map, lerp, getMousePos, calcWinSize, distance } from "./utils";
+export {
+  map,
+  clamp,
+  lerp,
+  dampingFactor,
+  getMousePos,
+  calcWinSize,
+  distance,
+} from "./utils";
