a018d7bea2a8e876ed066a99cc5d016a95fb9ffa
AntoineMeunier52 <meunier.antoine52@gmail.com>
2026-06-03 23:52:53 +0200

    feat: refactor Card class to improve mouse event handling and corner position calculation

--- galeries/QuadCursor/card.ts
+++ galeries/QuadCursor/card.ts
@@ -1,18 +1,46 @@
 import { EventEmitter } from "@lib";
-import { getMousePos, distance, lerpFactored, map, clamp } from "@lib";
+import {
+  getMousePos,
+  distance,
+  calcWinSize,
+  lerpFactored,
+  map,
+  clamp,
+} from "@lib";
+
+let mousePos = { x: 0, y: 0 };
+document.addEventListener("mousemove", (e) => (mousePos = getMousePos(e)));
+
+let windowSize = calcWinSize();
+window.addEventListener("resize", () => (windowSize = calcWinSize()));
 
 export class Card extends EventEmitter<{
-  "quad-click": { x: number; y: number };
+  "quad-click": { x: number; y: number }[];
 }> {
   private el: HTMLElement;
   private state: "idle" | "hovered" | "clicked" = "idle";
+  private corners: { x: number; y: number }[] = [];
+  private rect: DOMRect;
 
   constructor(el: HTMLElement) {
     super();
     this.el = el;
+    this.rect = this.el.getBoundingClientRect();
+    this.corners = this.calcCornersPos();
 
-    this.el.addEventListener("mouseenter", this.onMouseEnter);
-    this.el.addEventListener("mouseleave", this.onMouseLeave);
-    //this.el.addEventListener("click", this.onClick);
+    window.addEventListener("resize", () => {
+      this.rect = this.el.getBoundingClientRect();
+      this.corners = this.calcCornersPos();
+    });
+  }
+
+  calcCornersPos() {
+    const { left, top, width, height } = this.rect;
+    return [
+      { x: left, y: top },
+      { x: left + width, y: top },
+      { x: left, y: top + height },
+      { x: left + width, y: top + height },
+    ];
   }
 }
